f97e891cbd542054279ffc30587ffa79a2e98059
sabin ahmed <sabinahmed786@gmail.com>
2024-05-01 22:53:47 -0400

    first commit...

---
+++ src/git/Github.java
@@ -0,0 +1,25 @@
+package git;
+
+public class Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+			
+			int a = 0 ;
+			int b = 8;
+			System.out.println(b/a);
+			
+		}catch(Throwable E) {
+			System.out.println(E.getMessage());
+			System.out.println(E.getCause());
+			System.out.println(E.getStackTrace());
+			E.printStackTrace();
+			
+		}
+	
+		
+	}
+
+}
